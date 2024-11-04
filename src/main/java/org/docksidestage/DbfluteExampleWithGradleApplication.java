90cef711807febf2ac98ca772d926e3bdbc62e15
Shimizu, Yuko <shimizu.y.af@gmail.com>
2024-11-04 22:48:28 +0900

    Initial commit

---
+++ src/main/java/org/docksidestage/DbfluteExampleWithGradleApplication.java
@@ -0,0 +1,13 @@
+package org.docksidestage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbfluteExampleWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbfluteExampleWithGradleApplication.class, args);
+	}
+
+}
